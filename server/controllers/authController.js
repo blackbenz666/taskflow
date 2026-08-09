a15eabad35c6aa9aa83bb2aa94fef85a22be89fa
kirushaRetar <kevincot2@gmail.com>
2026-08-09 16:16:50 +0500

    feat(server): add Middleware for auth, getMe method

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -68,3 +68,16 @@ export const loginUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "Пользователь не найден" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
