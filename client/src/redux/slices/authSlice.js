aa561fcd3f4dbbc5ca652131c350d7ce7d237913
kirushaRetar <kevincot2@gmail.com>
2026-08-09 19:50:47 +0500

    feat(client): setup Redux store, authSlice, and axios instance

---
+++ client/src/redux/slices/authSlice.js
@@ -0,0 +1,120 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { act } from "react";
+
+export const registerUser = createAsyncThunk(
+  "auth/registerUser",
+  async (userData, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post("/auth/register", userData);
+
+      if (data.token) {
+        localStorage.setItem("token", data.token);
+      }
+
+      return data;
+    } catch (error) {
+      return rejectWithValue(
+        error.response?.data?.message || "Ошибка регистрации",
+      );
+    }
+  },
+);
+
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async (userData, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post("/auth/login", userData);
+
+      if (data.token) {
+        localStorage.setItem("token", data.token);
+      }
+
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data?.message || "Ошибка входа");
+    }
+  },
+);
+
+export const getMe = createAsyncThunk(
+  "auth/getMe",
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get("/auth/me");
+
+      if (!data) console.log("me: Нет данных");
+
+      return data;
+    } catch (error) {
+      return rejectWithValue(
+        error.response?.data?.message ||
+          "Не удалось получить данные о пользователе",
+      );
+    }
+  },
+);
+
+const initialState = {
+  user: null,
+  token: localStorage.getItem("token") || null,
+  isLoading: false,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+      localStorage.removeItem("token");
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload;
+        state.token = action.payload.token;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(loginUser.pending, (state, action) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload;
+        state.token = action.payload.token;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(getMe.pending, (state, action) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(getMe.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload;
+      })
+      .addCase(getMe.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { logout } = authSlice.actions;
+export default authSlice.reducer;
